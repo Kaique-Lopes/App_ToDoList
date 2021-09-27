89a539e3367332b86a3645dc1e7c06c8087883a6
Kaique <kaique.lopes3271@gmail.com>
2021-09-27 10:16:46 -0300

    indexPath.section

--- App_ToDoList/App_ToDoList/Controllers/Cells/CreateTasksViewController.swift
+++ App_ToDoList/App_ToDoList/Controllers/Cells/CreateTasksViewController.swift
@@ -42,6 +42,13 @@ class CreateTasksViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "TaskDescriptionCell", for: indexPath) as! TaskDescriptionTableViewCell
+            return cell
         }
+        if indexPath.section == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+            return cell
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DateCell", for: indexPath) as! DateTimeTableViewCell
+        return cell
     }
 }
